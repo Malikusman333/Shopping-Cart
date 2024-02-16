cfa719b4de0e869c9ee5587d50190f3142eff7ea
Muhammad Usman <153406646+Malikusman333@users.noreply.github.com>
2024-02-17 02:43:48 +0500

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,110 @@
+/*============ Build our products data ==============*/
+
+export const productsData = [
+  {
+    id: 1,
+    title: "Fish",
+    price: 2000,
+    imageUrl:
+      "./images/Fish.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Nihari",
+    price: 300,
+    imageUrl: "./images/Nihari.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Dish 3",
+    price: 300,
+    imageUrl: "./images/",
+  },
+
+  {
+    id: 4,
+    title: "Dish 4",
+    price: 95,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 5,
+    title: "Dish 5",
+    price: 3500,
+    imageUrl:
+      "./images/",
+  },
+
+  {
+    id: 6,
+    title: "Dish 6",
+    price: 12,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 7,
+    title: "Dish 7",
+    price: 500,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 8,
+    title: "Dish 8",
+    price: 500,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 9,
+    title: "Dish 9",
+    price: 129,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 10,
+    title: "Dish 10",
+    price: 650,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 11,
+    title: "Dish 11",
+    price: 700,
+    imageUrl: "./images",
+  },
+
+  {
+    id: 12,
+    title: "BBQ",
+    price: 119,
+    imageUrl: "./images/BBQ.jpg",
+  },
+
+  {
+    id: 13,
+    title: "Dish 13",
+    price: 400,
+    imageUrl: "./images/",
+  },
+
+  {
+    id: 14,
+    title: "Dish 14",
+    price: 500,
+    imageUrl: "./images/",
+  },
+
+  {
+    id: 15,
+    title: "Dish 15",
+    price: 350,
+    imageUrl: "./images",
+  },
+];
